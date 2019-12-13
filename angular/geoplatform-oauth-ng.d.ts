7067e06a5eda6676d1176bfa5e07be3bd3a78988
Forest Gafford <forestg@imagemattersllc.com>
2019-12-13 13:43:36 -0500

    Cleanup: rearrange strucrture to resemble client-api

--- angular/geoplatform-oauth-ng.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Generated bundle index. Do not edit.
- */
-export * from './index';
-export { msgProvider as ɵc } from './angular/index';
-export { DefaultAuthConf as ɵa } from './auth';
-export { JWT as ɵd, Messenger as ɵb } from './authTypes';
